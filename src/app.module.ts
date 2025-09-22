fa72883371c39c77581c2e9878aa8400f91d848a
Juan Medina <jmedinaguerrero847@gmail.com>
2025-09-22 18:52:12 -0500

    Enhance NestJS configuration with environment handling and database setup. Added TypeORM and configuration modules, updated start scripts, and introduced new types for API responses and environment variables.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { DataSourceConfig } from './config/data.source';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath:
+        process.env.NODE_ENV === 'development'
+          ? `.env.${process.env.NODE_ENV.trim()}`
+          : '.env.production',
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRoot(DataSourceConfig),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
